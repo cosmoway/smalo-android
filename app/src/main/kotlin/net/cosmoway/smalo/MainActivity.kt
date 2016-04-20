0c63388d398e2ddda033dffa89b709df2cd42804
ANDO <ando@cosmoway.net>
2016-04-20 09:22:11 +0900

    デザインの大まかな実装。

--- app/src/main/kotlin/net/cosmoway/smalo/MainActivity.kt
+++ app/src/main/kotlin/net/cosmoway/smalo/MainActivity.kt
@@ -1,7 +1,9 @@
 package net.cosmoway.smalo
 
 import android.Manifest
-import android.app.ListActivity
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
@@ -9,34 +11,39 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.support.v4.app.ActivityCompat
+import android.util.Log
+import android.view.View
 import android.view.WindowManager
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 
 
-class MainActivity : ListActivity() {
+class MainActivity : Activity(), View.OnClickListener {
 
     //スリープモードからの復帰の為のフラグ定数
     private val FLAG_KEYGUARD = WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
     private var mReceiver: SesameBroadcastReceiver? = null
     private var mIntentFilter: IntentFilter? = null
+    private var mStartButton: Button? = null
+    private var mStopButton: Button? = null
+    private var mLockButton: ImageButton? = null
+    private var mAnimatorSet1: AnimatorSet? = null
+    private var mAnimatorSet2: AnimatorSet? = null
+    private var mAnimatorSet3: AnimatorSet? = null
+    private var mOval1: ImageView? = null
+    private var mOval2: ImageView? = null
+    private var mOval3: ImageView? = null
 
     // サービスから値を受け取ったら動かしたい内容を書く
-    private val updateHandler = object : Handler() {
-        override fun handleMessage(msg: Message) {
+    /*private val updateHandler = object : Handler() {
+         override fun handleMessage(msg: Message) {
 
-            val bundle = msg.data
-            val message: Array<String> = bundle.getStringArray("state")
-            val lv: ListView = findViewById(R.id.list1) as ListView
-            val adapter: ArrayAdapter<String> = ArrayAdapter(this@MainActivity,
-                    android.R.layout.simple_list_item_1, message)
-            listAdapter = adapter
-            lv.adapter
-        }
-    }
+             val bundle = msg.data
+         }
+     }*/
 
     private fun requestLocationPermission() {
         // 位置情報サーヴィス を利用可能か
@@ -53,8 +60,26 @@ class MainActivity : ListActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mOval1 = findViewById(R.id.oval1) as ImageView
+        mOval2 = findViewById(R.id.oval2) as ImageView
+        mOval3 = findViewById(R.id.oval3) as ImageView
+        mStartButton = findViewById(R.id.btn_start) as Button
+        mStopButton = findViewById(R.id.btn_stop) as Button
+        mLockButton = findViewById(R.id.btn_lock) as ImageButton
+        mStartButton?.setOnClickListener(this)
+        mStopButton?.setOnClickListener(this)
+        mLockButton?.setOnClickListener(this)
+
+        mAnimatorSet1 = AnimatorInflater.loadAnimator(this, R.animator.anim_oval1) as AnimatorSet;
+        mAnimatorSet2 = AnimatorInflater.loadAnimator(this, R.animator.anim_oval2) as AnimatorSet;
+        mAnimatorSet3 = AnimatorInflater.loadAnimator(this, R.animator.anim_oval3) as AnimatorSet;
+        (mAnimatorSet1 as AnimatorSet).setTarget(mOval1);
+        (mAnimatorSet2 as AnimatorSet).setTarget(mOval2);
+        (mAnimatorSet3 as AnimatorSet).setTarget(mOval3);
+
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "BLE未対応端末です", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "ママのオッパイでも吸ってろw", Toast.LENGTH_SHORT).show(); //ウソです。
+            Toast.makeText(this, "BLE非対応端末です", Toast.LENGTH_SHORT).show();
             finish();
         }
 
@@ -76,7 +101,7 @@ class MainActivity : ListActivity() {
         (mIntentFilter as IntentFilter).addAction("UPDATE_ACTION")
         registerReceiver(mReceiver, mIntentFilter)
 
-        (mReceiver as SesameBroadcastReceiver).registerHandler(updateHandler)
+        //(mReceiver as SesameBroadcastReceiver).registerHandler(updateHandler)
     }
 
     override fun onStop() {
@@ -96,12 +121,45 @@ class MainActivity : ListActivity() {
             return
         }
 
-        startService(Intent(this, SesameBeaconService::class.java))
+        //startService(Intent(this@MainActivity, SesameBeaconService::class.java))
         //stopService(Intent(this, SesameBeaconService::class.java))
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(mReceiver)
+        if (mAnimatorSet1 != null) {
+            mAnimatorSet1?.end()
+            mAnimatorSet1 = null
+        }
+        if (mAnimatorSet2 != null) {
+            mAnimatorSet2?.end()
+            mAnimatorSet2 = null
+        }
+        if (mAnimatorSet3 != null) {
+            mAnimatorSet3?.end()
+            mAnimatorSet3 = null
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v == mStartButton) {
+            Log.d("Button", "Start")
+            startService(Intent(this, SesameBeaconService::class.java))
+            mAnimatorSet1?.start()
+            mAnimatorSet2?.start()
+            mAnimatorSet3?.start()
+        } else if (v == mStopButton) {
+            Log.d("Button", "Stop")
+            stopService(Intent(this, SesameBeaconService::class.java))
+            mAnimatorSet1?.end()
+            mAnimatorSet2?.end()
+            mAnimatorSet3?.end()
+        } else if (v == mLockButton) {
+            Log.d("Button", "Lock")
+            mAnimatorSet1?.start()
+            mAnimatorSet2?.start()
+            mAnimatorSet3?.start()
+        }
     }
 }
