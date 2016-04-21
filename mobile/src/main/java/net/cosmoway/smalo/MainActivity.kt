320a46080851df304f65b9355449837a48fb388e
ANDO <ando@cosmoway.net>
2016-04-21 16:32:18 +0900

    mobile:アクティビティクラス名変更。

--- mobile/src/main/java/net/cosmoway/smalo/MainActivity.kt
+++ mobile/src/main/java/net/cosmoway/smalo/MainActivity.kt
@@ -1,15 +1,13 @@
 package net.cosmoway.smalo
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
 
-class HandheldActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("onCreate", "実行")
-        this.setContentView(R.layout.activity_handheld)
+        this.setContentView(R.layout.activity_main)
     }
 
     override fun onResume() {
