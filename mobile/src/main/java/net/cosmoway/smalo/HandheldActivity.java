e9af96ba50fb2921ef05ddeaf52a149260dacaee
ANDO <ando@cosmoway.net>
2016-04-21 16:07:27 +0900

    パッケージ名変更、ライブラリ更新。

---
+++ mobile/src/main/java/net/cosmoway/smalo/HandheldActivity.java
@@ -0,0 +1,26 @@
+package net.cosmoway.smalo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class HandheldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("onCreate", "実行");
+        this.setContentView(R.layout.activity_handheld);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+}
