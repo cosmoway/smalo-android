ecf63dd93e1406407df6f6e713d10ab79310a784
ANDO <ando@cosmoway.net>
2016-05-12 09:25:44 +0900

    サービスクラス：コードをリファクタリングしました。

--- mobile/src/main/kotlin/net/cosmoway/smalo/MyService.kt
+++ mobile/src/main/kotlin/net/cosmoway/smalo/MyService.kt
@@ -55,91 +55,16 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
     private var mApiClient: GoogleApiClient? = null
     private var mIsUnlocked: Boolean? = null
     private var mReceivedMessageFromWear: String? = null
-    private var mHashValue: String? = null
-    // Nsd Manager
-    private var mNsdManager: NsdManager? = null
     // UUID設定用
-    private var mId: String? = null      //タイマタスククラス
-    private var mHandler: Handler = Handler();   //UI Threadへのpost用ハンドラ
+    private var mId: String? = null
 
 
     companion object {
-        val TAG_SERVICE = "MyService"
+        private val TAG_SERVICE = "MyService"
         private val TAG_API = "API"
-        val TAG_NSD = "NSD"
-        val SERVICE_TYPE = "_xdk-app-daemon._tcp."
-        val MY_SERVICE_NAME = "smalo"
-        //val MY_SERVICE_NAME = "smalo-dev"
-        val MY_SERVICE_UUID = "51a4a738-62b8-4b26-a929-3bbac2a5ce7c"
-        val MY_APP_NAME = "SMALO"
-    }
-
-    private fun toEncryptedHashValue(algorithmName: String, value: String): String {
-        var md: MessageDigest? = null
-        try {
-            md = MessageDigest.getInstance(algorithmName)
-        } catch (e: NoSuchAlgorithmException) {
-            e.printStackTrace()
-        }
-
-        val sb: StringBuilder = StringBuilder()
-        md!!.update(value.toByteArray())
-        for (b in md.digest()) {
-            val hex = String.format("%02x", b)
-            sb.append(hex)
-        }
-        return sb.toString()
-    }
-
-    private fun getRequest(url: String) {
-        object : AsyncTask<Void?, Void?, String?>() {
-            override fun doInBackground(vararg params: Void?): String? {
-                var result: String
-                // リクエストオブジェクトを作って
-                val request: Request = Request.Builder().url(url).get().build()
-
-                // クライアントオブジェクトを作って
-                val client: OkHttpClient = OkHttpClient()
-
-                // リクエストして結果を受け取って
-                try {
-                    val response = client.newCall(request).execute()
-                    result = response.body().string()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    return "Connection Error"
-                }
-                // 返す
-                Log.d(TAG_SERVICE, result)
-                return result
-            }
-
-            override fun onPostExecute(result: String?) {
-                if (result != null) {
-                    if (result.equals("locked") || result.equals("unlocked") || result.equals("unknown")) {
-                        sendDataByMessageApi(result)
-                        sendBroadCast(result)
-                        mState = result
-                        if (result == "locked" && mIsUnlocked == false && mIsBackground == true) {
-                            //TODO:開処理リクエスト。
-                            //getRequest("http:/$mHost:10080/api/locks/unlocking/$mHashValue")
-                        }
-                    } else {
-                        if (result.equals("200 OK")) {
-                            sendDataByMessageApi(result)
-                            sendBroadCast(result)
-                            val uri: Uri = RingtoneManager
-                                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-                            val ringtone: Ringtone = RingtoneManager
-                                    .getRingtone(applicationContext, uri)
-                            ringtone.play()
-                            mIsUnlocked = true
-                        }
-                        makeNotification(result)
-                    }
-                }
-            }
-        }.execute()
+        //private val MY_SERVICE_NAME = "smalo-dev"
+        private val MY_SERVICE_UUID = "51a4a738-62b8-4b26-a929-3bbac2a5ce7c"
+        private val MY_APP_NAME = "SMALO"
     }
 
     // TODO: 状態。
@@ -170,13 +95,7 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
                 notificationIntent, 0)
 
         builder.setContentTitle(title) // 1行目
-        if (title.equals("Connection Error")) {
-            builder.setContentText("通信処理が正常に終了されませんでした。\n通信環境を御確認下さい。")
-        } else if (title.indexOf("400") != -1) {
-            builder.setContentText("予期せぬエラーが発生致しました。\n開発者に御問合せ下さい。")
-        } else if (title.indexOf("403") != -1) {
-            builder.setContentText("認証に失敗致しました。\nシステム管理者に登録を御確認下さい。")
-        } else if (title.equals("Enter Region")) {
+        if (title.equals("Enter Region")) {
             builder.setContentText("領域に入りました。")
         } else if (title.equals("Exit Region")) {
             builder.setContentText("領域から出ました。")
@@ -187,66 +106,6 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
 
         val manager = NotificationManagerCompat.from(applicationContext)
         manager.notify(1, builder.build())
-    }
-
-    fun ensureSystemServices() {
-        mNsdManager = getSystemService(Service.NSD_SERVICE) as NsdManager
-        /*if (nsdManager == null) {
-            return
-        }*/
-    }
-
-    private fun startDiscovery() {
-        mNsdManager?.discoverServices(SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, MyDiscoveryListener())
-    }
-
-    private fun stopDiscovery() {
-        mNsdManager?.stopServiceDiscovery(MyDiscoveryListener())
-    }
-
-    private inner class MyDiscoveryListener : NsdManager.DiscoveryListener {
-        override fun onServiceFound(serviceInfo: NsdServiceInfo) {
-            Log.i(TAG_NSD, String.format("Service found serviceInfo=%s", serviceInfo))
-            if (serviceInfo.serviceType.equals(SERVICE_TYPE)) {
-                mNsdManager?.resolveService(serviceInfo, MyResolveListener())
-            }
-        }
-
-        override fun onDiscoveryStarted(serviceType: String) {
-            Log.i(TAG_NSD, String.format("Discovery started serviceType=%s", serviceType))
-        }
-
-        override fun onDiscoveryStopped(serviceType: String) {
-            Log.i(TAG_NSD, String.format("Discovery stopped serviceType=%s", serviceType))
-        }
-
-        override fun onServiceLost(serviceInfo: NsdServiceInfo) {
-            Log.i(TAG_NSD, String.format("Service lost serviceInfo=%s", serviceInfo))
-        }
-
-        override fun onStartDiscoveryFailed(serviceType: String, errorCode: Int) {
-            Log.w(TAG_NSD, String.format("Failed to start discovery serviceType=%s, errorCode=%d",
-                    serviceType, errorCode))
-        }
-
-        override fun onStopDiscoveryFailed(serviceType: String, errorCode: Int) {
-            Log.w(TAG_NSD, String.format("Failed to stop discovery serviceType=%s, errorCode=%d",
-                    serviceType, errorCode))
-        }
-    }
-
-    private inner class MyResolveListener : NsdManager.ResolveListener {
-        override fun onServiceResolved(serviceInfo: NsdServiceInfo) {
-            Log.i(TAG_NSD, String.format("Service resolved serviceInfo=%s", serviceInfo.host))
-            if (serviceInfo.serviceName == MY_SERVICE_NAME) {
-                mHost = serviceInfo.host.toString()
-            }
-        }
-
-        override fun onResolveFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
-            Log.w(TAG_NSD, String.format("Failed to resolve serviceInfo=%s, errorCode=%d",
-                    serviceInfo, errorCode))
-        }
     }
 
     // TODO:状態
@@ -261,25 +120,11 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG_SERVICE, "created")
-        connectIfNeeded()
         mIsUnlocked = false
         mIsBackground = true
 
-        // TODO:端末固有識別番号読出
-        val sp = PreferenceManager.getDefaultSharedPreferences(this)
-        mId = sp?.getString("saveId", null)
-        if (mId == null) {
-            Log.d("id", "null")
-            // 端末固有識別番号取得
-            // mId = UUID.randomUUID().toString()
-            mId = "2df60388-e96e-4945-93d0-a4836ee75a3c" //ando
-
-            sp?.edit()?.putString("saveId", mId)?.apply()
-        }
-
         mActivity = MobileActivity()
         mActivity?.setCallback(this)
-        Log.d("id", mId)
         //sendJson("{\"uuid\":\"$mId\"}")
         //BTMのインスタンス化
         /*mBeaconManager = BeaconManager.getInstanceForApplication(this)
@@ -447,7 +292,7 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
             Log.d(TAG_SERVICE, "receivedMessage: $mReceivedMessageFromWear")
 
             // TODO: 取得した内容に応じ処理
-            // TODO:問い合わせ要求時
+            // TODO: 問い合わせ要求時
             if (mReceivedMessageFromWear.equals("getState")) {
                 //TODO: 鍵の情報の取得
                 Log.d(TAG_SERVICE, "getState")
@@ -458,12 +303,12 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
                     sendBroadCast("unknown")
                     sendDataByMessageApi("unknown")
                 }
-                // TODO:解錠施錠要求時
+                // TODO: 解錠施錠要求時
             } else if (mReceivedMessageFromWear.equals("stateUpdate")) {
                 Log.d(TAG_API, "locking")
                 //TODO: 今のステートに応じて処理する。Wearに結果返すのは解錠施錠時。
                 if (mState.equals("locked")) {
-                    //TODO:開処理リクエスト。
+                    //TODO: 開処理リクエスト。
                     Log.d(TAG_SERVICE, "unlocking");
                     sendJson("{\"command\":\"unlock\"}")
                 } else if (mState.equals("unlocked")) {
@@ -534,9 +379,8 @@ class MyService : WearableListenerService(), BeaconConsumer, BootstrapNotifier, 
 
     private fun disconnect() {
         Log.d(TAG_SERVICE, "disconnect")
-        if (mWebSocketClient != null) {
-            (mWebSocketClient as MyWebSocketClient).close()
-            mWebSocketClient = null
-        }
+        mWebSocketClient?.close()
+        mWebSocketClient = null
     }
 }
+
