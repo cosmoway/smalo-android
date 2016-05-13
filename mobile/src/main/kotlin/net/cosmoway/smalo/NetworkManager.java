c54d2f27aaad68559ec2414a665b32a8bad89c97
ANDO <ando@cosmoway.net>
2016-05-13 16:51:56 +0900

    サポートライブラリ更新。

--- mobile/src/main/kotlin/net/cosmoway/smalo/NetworkManager.java
+++ mobile/src/main/kotlin/net/cosmoway/smalo/NetworkManager.java
@@ -1,7 +1,19 @@
 package net.cosmoway.smalo;
 
-/**
- * Created by ando on 2016/05/13.
- */
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 public class NetworkManager {
+
+    /**
+     * ネットワーク接続が確立されているか
+     */
+    public static boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && connectivityManager.getActiveNetworkInfo().isConnected();
+
+    }
 }
